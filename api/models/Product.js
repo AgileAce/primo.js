cf1544841dce71826bc6156bb14966b28b689e73
Casper Du Plessis <casper@Cass-MacBook-Pro.local>
2014-03-03 16:27:18 +0200

    Adding EOD changes for 3 March

---
+++ api/models/Product.js
@@ -0,0 +1,20 @@
+/**
+ * Product.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+    adapter: 'mongo',
+    attributes: {
+        name: {
+            type: 'string',
+            required: true,
+            maxLength: 20
+        },
+        logo: {
+            type: 'string'
+        }
+    }
+};
